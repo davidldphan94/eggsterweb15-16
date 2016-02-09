7770788537ee8b0637fefb3e46b9e7b0ea38b4f4
David Phan <davidldphan94@gmail.com>
2016-02-09 00:16:44 -0800

    update

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+    <title>Eggster at Berkeley</title>
+    <link href="css/stylesheet.css" rel="stylesheet">
+    <link href="js/stylesheet.css" rel="stylesheet">
+</head>
+    <body>
+        <!-- Navigation -->
+        <div class="navigation">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="getinvolved.php">Get Involved</a></li>
+                <li><a href="sponsor.php">Sponsors</a></li>
+                <li><a href="beneficiaries.php">Beneficiaries</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="calendar.php">Calendar</a></li>
+            </ul>
+
+        </div>
+        <!-- Content -->
+        <div>
+                <h3>WELCOME TO EGGSTER</h3>
+
+                <p>
+                Eggster is a student organization sponsored by ASUC, a 501(c)(3) nonprofit organization located in Berkeley. Every year we organize and run the Eggster Egghunt and Learning Festival, the largest free egg hunt and learning festival in Northern California. Not only is it free for all kids, it is also a learning festival filled with educational booths and activities! In addition, we partner with local charities to support youth education. 
+                
+                The 2016 Eggster Learning Festival is going to be on April 2nd, 2016. Please check the <a href="eventday.php">Event Day</a> page for more details.
+                </p>
+                <b><p>
+                    THE FALL 2015 BENEFICIARY APPLICATION IS NOW OPEN! To become a beneficiary for this year's learning festival, 
+                    please apply via <a href="http://goo.gl/forms/HXdkSsb22t">this link.</a><br><br></p></b>
+                </p>
+        </div> 
+    </body>
+</html>
---
+++ officers.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eggster</title>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/jquery.jcarousel.css" type="text/css" media="all" />
+    <link rel="shortcut icon" href="css/images/bunny.ico" />
+    <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery.jcarousel.pack.js"></script>
+    <script type="text/javascript" src="js/func.js"></script>
+</head>
+	<body>
+
+<div class="shell">
+    <div class="border">
+        <div id="header">
+            <h1 id="logo"><a href="index.php" class="notext">Eggster</a></h1>
+            <div class="socials right">
+                <ul>
+                    <li><a href="https://www.facebook.com/pages/Eggsters-Annual-Egghunt-Learning-Festival/575101285835697" target="_blank" class="fb"></a></li>
+                    <li class="last"><a href="http://twitter.com/Eggster_Cal" target="_blank" class="twit"></a></li>
+                </ul>
+            </div>
+            <div class="cl">&nbsp;</div>
+        </div>
+        
+        <div id="navigation">
+            <ul>
+                <li><a href="" class="active">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="getinvolved.php">Get Involved</a></li>
+                <li><a href="sponsor.php">Sponsors</a></li>
+                <li><a href="beneficiaries.php">Beneficiaries</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="eventday.php">Event Day</a></li>
+                <li><a href="calendar.php">Calendar</a></li>
+            </ul>
+            <div class="cl">&nbsp;</div>
+        </div>
+        
+        
+        <div id="main">
+            <div id="content" class="left">
+                <h3>Officers</h3>
+                
+
+                <div class="projects">                    
+                    <div class="item">
+                        <div class="image left">
+                            <a href="#"><img src="css/images/oski.jpg" alt="" /></a>
+                        </div>
+
+                        <div class="text left">
+                 </div>
+
+                        <div class="cl">&nbsp;</div>
+                
+                    </div>
+                
+                    
+                    <div class="info">
+                    <h3></h3> <center>
+                    <p>Eggster is currently sponsored by the ASUC, 501(c)(3) nonprofit organizations and UC Berkeley's Public Service Center. <br />Copyright &copy; Eggster 1994-2015 </center>  </p>
+                    </div>  
+                    
+                </div>
+            </div>
+
+
+          
+            <div class="cl">&nbsp;</div>
+        </div>
+        
+        <div class="shadow-l"></div>
+        <div class="shadow-r"></div>
+        <div class="shadow-b"></div>
+    </div>
+
+    <div id="footer">
+        <div class="footer-nav left">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="getinvolved.php">Get Involved</a></li>
+                <li><a href="sponsor.php">Sponsors</a></li>
+                <li><a href="beneficiaries.php">Beneficiaries</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="eventday.php">Event Day</a></li>
+                <li class="last"><a href="calendar.php">Calendar</a></li>
+            </ul>
+        </div>
+        <p class="copy right">Copyright &copy; Eggster 2015 | Valid CSS &amp; HTML.</p>
+        <div class="cl">&nbsp;</div>
+    </div>
+	</body>
+</html>
+
+<div class="shell">
+    <div class="border">
+        <div id="header">
+            <h1 id="logo"><a href="index.php" class="notext">Eggster</a></h1>
+            <div class="socials right">
+                <ul>
+                    <li><a href="https://www.facebook.com/pages/Eggsters-Annual-Egghunt-Learning-Festival/575101285835697" target="_blank" class="fb"></a></li>
+                    <li class="last"><a href="http://twitter.com/Eggster_Cal" target="_blank" class="twit"></a></li>
+                </ul>
+            </div>
+            <div class="cl">&nbsp;</div>
+        </div>
+        
+        <div id="navigation">
+            <ul>
+                <li><a href="" class="active">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="getinvolved.php">Get Involved</a></li>
+                <li><a href="sponsor.php">Sponsors</a></li>
+                <li><a href="beneficiaries.php">Beneficiaries</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="eventday.php">Event Day</a></li>
+                <li><a href="calendar.php">Calendar</a></li>
+            </ul>
+            <div class="cl">&nbsp;</div>
+        </div>
+        
+        <div class="slider">
+            <div class="slider-nav">
+                <a href="#" class="left notext">1</a>
+                <a href="#" class="left notext">2</a>
+                <a href="#" class="left notext">3</a>
+                <a href="#" class="left notext">4</a>
+                <div class="cl">&nbsp;</div>
+            </div>
+            <ul>
+                <li>
+                    <div class="item">
+                        <img src="css/images/p01.jpg" alt="" />
+                    </div>
+                </li>
+                <li>
+                    <div class="item">
+                        <img src="css/images/p02.jpg" alt="" />
+                    </div>
+                </li>
+                <li>
+                    <div class="item">
+                        <img src="css/images/p03.jpg" alt="" />
+                    </div>
+                </li>
+                <li>
+                    <div class="item">
+                        <img src="css/images/newgroup.jpg" alt="" />
+                    </div>
+                </li>
+            </ul>
+        </div>
+        
+        <div id="main">
+            <div id="content" class="left">
+                <div class="highlight">
+                    <h3>Welcome to Eggster</h3>
+                    <!-- <img src="css/images/about02.jpg" alt="" class="right" /> -->
+                    <p>Eggster is a student organization sponsored by ASUC, a 501(c)(3) nonprofit organization located in Berkeley. Every year we organize and run the Eggster Egghunt and Learning Festival, the largest free egg hunt and learning festival in Northern California. Not only is it free for all kids, it is also a learning festival filled with educational booths and activities! In addition, we partner with local charities to support youth education. 
+                    <br><br>The 2016 Eggster Learning Festival is going to be on April 2nd, 2016. Please check the <a href="eventday.php">Event Day</a> page for more details.
+                    <br><br>
+                    <!--<p>The egg hunts will take place <b>throughout the day</b> so feel free to come whenever you want while our event is going on.</p>-->
+                    <b><br><p>THE FALL 2015 BENEFICIARY APPLICATION IS NOW OPEN! To become a beneficiary for this year's learning festival, 
+                    please apply via <a href="http://goo.gl/forms/HXdkSsb22t">this link.</a><br><br></p></b>
+                    </p>
+                    <!--<p>Eggster will have a silent auction again this year. For information on the silent auction prizes, please visit the <a href="eventday.php">Event Day</a> page for details.-->
+                    
+                </div>
+
+
+                <div class="projects">
+                    <h3>Join us at our General Meetings!</h3>
+                    
+                    <div class="item">
+                        <div class="image left">
+                            <a href="#"><img src="css/images/oski.jpg" alt="" /></a>
+                        </div>
+
+                        <div class="text left">
+                    <!--Message to Kayla or future SHA! Please do not delete this comment. So this front page is where most people will look for information.
+                    They will not see this note because I have it hidden. Anything in the "<!-.-" and "-.->" things (minus the . which I had to put in there to make sure it didn't do anything) is hidden.
+                    If you're using Sublime Text 2 to edit this (which you should!) it should appear grey'd out.
+                    If you feel like the hidden information needs to be shown, just remove the "<!-.-" and "-.->"" around it and it should show. In Sublime Text 2, it won't be greyed out anymore.
+
+                    Okay, so I have some things hidden right now. The announcement for when the event will be is hidden for now, but just remove the "<!-.-" and "-.->" to show it again.
+                    And change the information to reflect this year's event. You don't really need to have this showing until January. DO NOT FORGET.-->
+
+                    <!-- <h4>21st Annual Eggster Egghunt and Learning Festival</h4>
+                    <p> Mark your calendars for <b> April 4, 2015 </b>! <br/>
+                        Location: Campanile, UC Berkeley  <br/>
+                        Time: 10AM-3PM <br>
+                        This is a free event! No registration necessary! 
+                        <br><a href="eventday.php" class="more">Find out more</a><br><br> -->
+
+                    <!--<h4>Volunteer at our event:</h4>
+                    Our event would not be possible without the help of wonderful volunteers like yourself!<br>
+                    Go to <a href=http://tinyurl.com/eggform15>Volunteer Sign-Up</a> to sign up to volunteer! <br>
+                    The online orientation can be found here: <a href=http://tinyurl.com/eggslides15>Online Orientation</a>
+                    <br><br>-->
+
+                    <!--next-->
+
+                    <p> 
+                        <!--Make sure to keep this current! Change every semester-->
+                    Time: Wednesdays, 7:00pm-8:00pm
+                    <br>Location: 242 Dwinelle
+                    <!--Make a FB event and link it here. Change the below URL to the current FB event.-->
+                    <!--<br><a href="https://www.facebook.com/events/179787442230637/" target="_blank">Click here to RSVP on Facebook</a>-->
+                    
+                    </p>
+
+                    <!--<p><br>
+                    <h4>Propbuilding:</h4>
+                    Date: Saturdays and Sundays<br>
+                    Time: 10am-4pm<br>
+                    Location: Saturdays: 185 Barrows <br> Sundays: 151 Barrows <br> 
+                    We'd love it if you could come by and help for as long or as short as you can spare!-->
+
+                </div>
+
+                        <div class="cl">&nbsp;</div>
+                
+                    </div>
+                    <br><br>
+                    <h3>Useful Links</h3>
+                
+                    <ul>
+                        <li><a href="calendar.php">Calendar</a>
+                        <li><a href="http://www.facebook.com/pages/Eggsters-Annual-Egghunt-Learning-Festival/575101285835697">Like us on Facebook!</a></li>
+                        <li><a href="https://www.justgive.org/basket?acton=donate&ein=94-0294680&designation=Donate+to+Eggster" target="_blank">Donate to Eggster</a></li>
+                        <li><a href="eventday.php">Event Day Info & Directions (2015 Event)</a></li><br><br><br>
+
+                    </ul>
+                
+                    
+                    <div class="info">
+                    <h3></h3> <center>
+                    <p>Eggster is currently sponsored by the ASUC, 501(c)(3) nonprofit organizations and UC Berkeley's Public Service Center. <br />Copyright &copy; Eggster 1994-2015 </center>  </p>
+                    </div>  
+                    
+                </div>
+            </div>
+
+
+            <div id="sidebar" class="right">
+            
+    <!-- Facebook NewsFeed -->
+    <h3>RECENT NEWS</h3>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <div class="fb-page" data-href="https://www.facebook.com/EggsterOrganization?fref=ts" data-width="1300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true" data-show-posts="true">
+    <div class="fb-xfbml-parse-ignore">
+    <blockquote cite="https://www.facebook.com/EggsterOrganization?fref=ts">
+    <a href="https://www.facebook.com/EggsterOrganization?fref=ts">Eggster at Berkeley</a>
+    </blockquote></div>
+    </div>
+    <br><br><br>
+
+                    <a href="https://docs.google.com/spreadsheet/viewform?formkey=dG8zSGxleVlPS051YWQzcDV4XzF0aFE6MQ" target="_blank" class="btn-join">Join the mailing list</a><br><br>
+                <div id="fb-root"></div>
+
+
+
+
+                <a href="donate.php" class="btn-buy"><em>Donate</em></a>
+
+                <div class="info">
+                
+                </div>
+                <br /> 
+                <center>
+                <script src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+    new TWTR.Widget({
+      version: 2,
+      type: 'profile',
+      rpp: 3,
+      interval: 30000,
+      width: 250,
+      height: 300,
+      theme: {
+        shell: {
+          background: '#0096c4',
+          color: '#ffffff'
+        },
+        tweets: {
+          background: '#0096c4',
+          color: '#ffffff',
+          links: '#000000'
+        }
+      },
+      features: {
+        scrollbar: false,
+        loop: false,
+        live: false,
+        hashtags: true,
+        timestamp: true,
+        avatars: false,
+        behavior: 'all'
+      }
+    }).render().setUser('Eggster_Cal').start();
+    </script>
+
+    </center>
+        <center>
+        <a href="http://www.ocf.berkeley.edu">
+        <img src="images/ocfbadge_blue8.png" alt="Hosted by the OCF" style="border:0px;" />
+        </a>
+    </center>
+    <center>        
+        <a href="http://www.hitwebcounter.com/freecounterfeatures.php" target="_blank">
+        <img src="http://hitwebcounter.com/counter/counter.php?page=338926&style=0007&nbdigits=4&type=page&initCount=0" title="WebSite Widgets" Alt="WebSite Widgets"   border="0" >
+        </a>
+    </center>
+<br>
+
+            </div>
+            <div class="cl">&nbsp;</div>
+        </div>
+        
+        <div class="shadow-l"></div>
+        <div class="shadow-r"></div>
+        <div class="shadow-b"></div>
+    </div>
