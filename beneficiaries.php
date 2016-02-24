39d65119e3c641037cb22ed57c7e84e17217035a
David Phan <davidldphan94@gmail.com>
2016-02-23 18:50:24 -0800

    fluid

--- beneficiaries.php
+++ beneficiaries.php
@@ -39,7 +39,142 @@
     </nav>
 
 
+<div class="panel panel-default">
+  <div class="panel-body">
+                <div>
+                    <h3>Our Beneficiaries</h3><p>
+                    Eggster has donated thousands of dollars directly to preselected Bay Area agencies that work with children, primarily those that have special needs and/or are socio-economically disadvantaged. <br>
+<br>
+If you are part of a non-profit organization serving children in the East Bay and would like to apply for funds, you can download the following application and mail it to:
+</p>
+<br />
+Eggster Organization<br />
+University of California, Berkeley<br />
+112 Hearst Gym #4520<br />
+Berkeley, CA 94720-4520
+<br />
+<br />
+<!--
+FUTURE SHA: PLEASE DO NOT DELETE THIS COMMENT.
+Please upload new beneficiary documents. This is so potential organizations who PCR did not mail presonally who want to apply can apply. We've gotten some good beneficiaries that way.
+    How to upload documents:
+    1) Download the file.
+    2) Open WinSCP and double click on "misc" folder and then "docs" folder. This should open up the docs folder and you should see past files inside.
+    3) (optional) Delete old files if it seems too crowded.
+    4) Drag and drop files into this folder. Verify that the files are actually there.
+    5) Change the below URLs (for example, "./misc/docs/Eggster Beneficiary Application 2014.pdf") to reflect current files.
+        So let's say that the new beneficiary file is called Eggster Beneficiary App 2015.pdf
+        The URL below should read "./misc/docs/Eggster Beneficiary App 2015.pdf". The entire code should be: 
+        <a href="./misc/docs/Eggster Beneficiary Application 2014.pdf" target="_blank">2014 Beneficiary Application</a>
+    6) Test website to make sure the proper files are being linked.
+    7) If there's a problem, ask web-savvy or previous SHA for help.-->
+<!--2013-2014 Beneficiary Forms:<br />
+<a href="./misc/docs/Eggster Beneficiary Application 2014.pdf" target="_blank">2014 Beneficiary Application</a><br>
+<a href="./misc/docs/Eggster Beneficiary Cover Letter 2014.pdf" target="_blank">2014 Beneficiary Letter</a><br><br>-->
 
+
+
+            
+                <p>
+                    <!--Make sure this says if we're accepting applications or not. Add the deadline when PCR/Overalls tell you what it is.-->
+                Fall 2015 applications are currently <b>OPEN!</b> 
+                <br>To become a beneficiary for this year's learning festival, 
+                    please apply via <a href="http://goo.gl/forms/HXdkSsb22t">this link.</a><br><br>
+                </p>
+                <!-- <p>
+                The 2014-2015 Beneficiaries are <a href="http://www.boostoakland.org/" target="_blank">Boost! West Oakland</a> and <a href="http://www.rocksf.org/" target="_blank">R.O.C.K. SF</a>.
+                <br/> <br>Thank you!
+                </p> -->
+                <br />
+                <br />
+                <h3>Beneficiaries from previous years:</h3>
+
+<b>The 2014-2015 Beneficiaries</b><br />
+<a href="http://www.boostoakland.org/" target="_blank">Boost! West Oakland</a><br />
+<a href="http://www.rocksf.org/" target="_blank">R.O.C.K SF</a><br /><br>
+
+<b>The 2013-2014 Beneficiaries</b><br />
+<a href="http://oaklandyouthchorus.org" target="_blank">Oakland Youth Chorus</a><br />
+<a href="http://www.superstarsliteracy.org" target="_blank">Super Stars Literacy</a><br /><br>
+
+<b>The 2012-2013 Beneficiaries</b><br />
+<a href="http://www.coachart.org" target="_blank">CoachArt</a><br />
+<a href="http://www.missiongraduates.org" target="_blank">Mission Graduates</a><br /><br>
+
+                <b>The 2011-2012 Beneficiaries</b><br />
+<a href="http://www.alacostacenters.org/index.php" target="_blank">Ala Costa Center</a><br />
+<a href="http://www.firstgraduate.org/" target="_blank">First Graduate</a><br />
+<a href="http://www.contracostaarc.org/" target="_blank">George Miller Center</a><br /><br>
+            
+                <b>The 2010-2011 Beneficiaries</b><br />
+<a href="http://www.habitot.org/" target="_blank">Habitot</a><br />
+<a href="http://www.contracostaarc.org" target="_blank">George Miller Center</a><br /><br>
+<b>The 2009-2010 Beneficiaries</b><br />
+<a href="http://alacostacenter.org/" target="_blank">Ala Costa Center</a><br />
+<a href="http://www.contracostaarc.org" target="_blank">George Miller Center</a><br /><br>
+<b>The 2008-2009 Beneficiaries</b><br />
+<a href="http://www.ocf.berkeley.edu/~spa/" target="_blank">Berkeley's Student Parent Association for Recruitment and Retention (SPARR)</a><br />
+<a href="http://www.contracostaarc.org" target="_blank">George Miller Center</a><br />
+<a href="http://www.sports4kids.org/" target="_blank">Sports 4 Kids</a>
+<br />
+<br />
+<b>The 2007-2008 Beneficiaries</b><br />
+<a href="http://alacostacenter.org/" target="_blank">Ala Costa Center</a><br />
+<a href="http://www.habitot.org/" target="_blank">Habitot</a><br />
+<a href="http://www.sports4kids.org/" target="_blank">Sports 4 Kids</a>
+<br />
+<br />
+<b>The 2006-2007 Beneficiaries</b><br />
+<a href="http://www.fairyland.org/" target="_blank">Children&#39s Fairyland</a><br />
+<a href="http://alacostacenter.org/" target="_blank">Ala Costa Center</a><br />
+<a href="http://www.jclread.org/" target="_blank">Jewish Coalition for Literacy</a>
+<br />
+<br />
+<b>The 2005-2006 Beneficiaries</b><br />
+<a href="http://www.fairyland.org/" target="_blank">Children&#39s Fairyland</a><br />
+<a href="http://www.homeaway.org/" target="_blank">Home Away from Homelessness</a> (Read their thank you letter <a href="./docs/letter_2006homeaway.jpg" target="blank">here</a>.)<br />
+<a href="http://home.earthlink.net/~snapkids/index1.html" target="_blank">Special Needs Aquatic Program (SNAP)</a>
+<br />
+<br />
+<b>The 2004-2005 Beneficiaries</b><br />
+<a href="http://alacostacenter.org/" target="_blank">Ala Costa Center</a><br />
+<a href="http://www.bacn.jkmas.com/" target="_blank">Bay Area Crisis Nursery (BACN)</a><br />
+<a href="http://www.sfbgirlscouts.org/" target="_blank">Girl Scouts of the San Francisco Bay Area (GSSFBA) - Oakland G.I.F.T</a>
+<br />
+<br />
+<b>The 2003-2004 Beneficiaries</b><br />
+<a href="http://www.ceid.org/" target="_blank">Center for the Education of the Infant Deaf (CEID)</a><br />
+<a href="http://www.mocha.org/" target="_blank">Museum of Children's Art (MOCHA)</a><br />
+<a href="http://studentparents.berkeley.edu/" target="_blank">Student Parent Center/Agency (SPC/A)</a><br />
+<a href="http://www.lookingglass.org/" target="_blank">Through the Looking Glass</a><br />
+<a href="http://www.twinvalleylearningcenter.com/" target="_blank">Twin Valley Learning Center</a>
+<br />
+<br />
+<b>The 2002-2003 Beneficiaries</b><br />
+<a href="http://alacostacenter.org/" target="_blank">Ala Costa Center</a><br />
+<a href="http://www.bacn.jkmas.com/" target="_blank">Bay Area Crisis Nursery (BACN)</a><br />
+<a href="http://www.blindbabies.org/" target="_blank">Blind Babies Foundation</a><br />
+<a href="http://www.ceid.org/" target="_blank">Center for the Education of the Infant Deaf (CEID)</a><br />
+<a href="http://www.sports4kids.org/" target="_blank">Sports for Kids</a>
+<br />
+<br />
+<b>The 2001-2002 Beneficiaries</b><br />
+<a href="http://alacostacenter.org/" target="_blank">Ala Costa Center</a><br />
+<a href="http://www.bacn.jkmas.com/" target="_blank">Bay Area Crisis Nursery (BACN)</a><br />
+<a href="http://www.ceid.org/" target="_blank">Center for the Education of the Infant Deaf (CEID)</a><br />
+<a href="http://studentparents.berkeley.edu/" target="_blank">Student Parent Center/Agency (SPC/A)</a>
+                </div>
+                
+            </div>
+
+            <div class="cl">&nbsp;</div>
+        </div>
+        
+        <div class="shadow-l"></div>
+        <div class="shadow-r"></div>
+        <div class="shadow-b"></div>
+    </div>
+    
     <div class="panel-footer">
         <p>Copyright &copy; Eggster 2015-2016 | Valid CSS &amp; HTML.</p>
         <div class="cl">&nbsp;</div>
