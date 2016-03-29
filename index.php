4c9c6cbc7e41b4f6d0357c1de69223c9754b1a85
David Phan <davidldphan94@gmail.com>
2016-03-28 18:39:03 -0700

    small change

--- index.php
+++ index.php
@@ -2,36 +2,143 @@
 <html lang="en-US" xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 <head>
     <title>Eggster at Berkeley</title>
-    <link href="css/stylesheet.css" rel="stylesheet">
-    <link href="js/stylesheet.css" rel="stylesheet">
+    <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <script type="bootstrap/dist/js/bootstrap.css/*.js" rel="stylesheet"></script>
+    <script type="bootstrap/jq/*.jq"></script>
+    <script type="text/javascript" src="bootstrap/jq/jquery-1.12.1.js"></script>
+    <script type="text/javascript" src="bootstrap/dist/js/bootstrap.js"></script>
 </head>
     <body>
         <!-- Navigation -->
-        <div class="navigation">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="about.php">About Us</a></li>
-                <li><a href="getinvolved.php">Get Involved</a></li>
-                <li><a href="sponsor.php">Sponsors</a></li>
-                <li><a href="beneficiaries.php">Beneficiaries</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="calendar.php">Calendar</a></li>
-            </ul>
+        <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+                    <nav class="navbar navbar-default navbar-fixed-top">
+                    <div class="container-fluid">
+                        <ul class="nav nav-tabs nav-justified">
+                        <li role="presentation"><a href="">Home</a></li>
+         
+        
+            <li role="presentation" class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About Us <span class="caret"></span></a>
+                    <ul class="dropdown-menu"> 
+                        <li><a href="officers.php">Officer Spotlight</a></li>
+                        <li><a href="about.php">Our Organization</a></li>
+                    </ul>
+            </li>
+          
 
+
+            <li role="presentation" class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Get Involved <span class="caret"></span></a>
+                    <ul class="dropdown-menu"> 
+                        <li><a href="getinvolved.php#members">For Members</a></li>
+                        <li><a href="getinvolved.php#alumni">For Alumni</a></li>
+                        <li><a href="getinvolved.php#volunteer">For Volunteers</a></li>
+                        <li><a href="getinvolved.php#performer">For Performers</a></li>
+                        <li><a href="getinvolved.php#organization">For Student Organizations</a></li>
+                    </ul>
+            </li>
+
+
+            <li role="presentation"><a href="sponsor.php">Sponsors</a></li>
+            <li role="presentation"><a href="beneficiaries.php">Beneficiaries</a></li>
+            <li role="presentation"><a href="calendar.php">Calendar</a></li>
+            <li role="presentation"><a href="eventday.php">Event Day</a></li>
+            </ul> 
+    
         </div>
-        <!-- Content -->
-        <div>
-                <h3>WELCOME TO EGGSTER</h3>
+    </nav>
 
+
+
+        <!-- Content -->
+       
+
+        <div class="jumbotron">
+            <div class="container-fluid">
+                <div class="page-header">
+                    <h2>WELCOME TO EGGSTER!</h2>
+                </div>
                 <p>
-                Eggster is a student organization sponsored by ASUC, a 501(c)(3) nonprofit organization located in Berkeley. Every year we organize and run the Eggster Egghunt and Learning Festival, the largest free egg hunt and learning festival in Northern California. Not only is it free for all kids, it is also a learning festival filled with educational booths and activities! In addition, we partner with local charities to support youth education. 
-                
                 The 2016 Eggster Learning Festival is going to be on April 2nd, 2016. Please check the <a href="eventday.php">Event Day</a> page for more details.
                 </p>
-                <b><p>
-                    THE FALL 2015 BENEFICIARY APPLICATION IS NOW OPEN! To become a beneficiary for this year's learning festival, 
-                    please apply via <a href="http://goo.gl/forms/HXdkSsb22t">this link.</a><br><br></p></b>
+                <p>
+                Are you a volunteer interested in helping out at our event this year? Sign up <a href="www.tinyurl.com/eggform16">here</a>! Contact <a href="mailto:eggsterevent@gmail.com">eggsterevent@gmail.com</a> if you have any questions.
                 </p>
-        </div> 
+                <p>
+                We are pleased to announce that Habitot will be this year's beneficiary for this year's Eggster Egghunt and Learning Festival! Thank you to all those who applied.
+                </p>
+                <p>
+                    <a href="about.php">More About Eggster</a> 
+                </p>
+            </div>
+        </div>
+
+
+ 
+
+
+
+    <div class="container-fluid">
+
+
+        <div class="page-header">
+            <h2>Useful Links and Information</h2>
+        </div>
+            <div class="row">
+    <div class="col-sm-8 col-md-6">
+        <div class="thumbnail">
+        <div class="container-fluid">
+        <p>
+            <b>General Meetings:</b> Mondays, 7:00-8:00pm in 182 Dwinelle<br>
+            <b>Prop-Building Times:</b> Saturdays and Sundays, 10:00am-2:00pm in 104 Barrows<br>
+        </p>
+        <ul>
+        <li>
+            <a href="donate.php">Donate</a> to Eggster!
+        </li>
+        <li> 
+            Join the <a href="https://docs.google.com/forms/d/1pOZpfPTYh_tjUvJB68MSr7leqLiPCCahbTlpwCfOGaI/viewform?formkey=dG8zSGxleVlPS051YWQzcDV4XzF0aFE6MQ">Mailing List</a>
+        </li>
+        <li> 
+            <a href="https://www.facebook.com/EggsterOrganization?fref=ts">Facebook Page</a>
+        </li>
+        </ul>
+
+        </div>
+        </div>
+      </div>
+
+ 
+     <div class="col-sm-8 col-md-6">
+        <div class="thumbnail">
+        <div class="container-fluid">
+            <div class="fb-page" data-href="https://www.facebook.com/EggsterOrganization?fref=ts" data-tabs="timeline" data-width="1000" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/EggsterOrganization?fref=ts"><a href="https://www.facebook.com/EggsterOrganization?fref=ts">Eggster at Berkeley</a></blockquote></div></div>
+
+        </div>
+        </div>
+      </div>
+        
+        <br>
+    </div>
+
+
+
+    <!-- Footer -->
+    <div class="panel-footer">
+        <p>Copyright &copy; Eggster 2015-2016 | Valid Bootstrap CSS &amp; HTML.</p>
+        <div class="cl">&nbsp;</div>
+    </div>
+
+
+
     </body>
+
+
 </html>
